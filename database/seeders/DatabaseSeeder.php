3ff23db1a087edf7e8f1fa2e9896dfc37e54a817
Sandy <onlinesandeep13@gmail.com>
2022-10-05 12:57:32 +0530

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             CreateAdminUserSeeder::class,
-            PostPermissionTableSeederSeeder::class,
+            PermissionTableSeeder ::class,
         ]);
     }
 }
